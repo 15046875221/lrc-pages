08e113eeac3e882d30a17571b523ef13ce15d91d
liangruicheng <3048617694@qq.com>
2021-06-02 18:11:22 +0800

    commiy

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const lrcPages = require('..')
-
-// TODO: Implement module test
-test('lrc-pages', () => {
-  expect(lrcPages('w')).toBe('w@zce.me')
-  expect(lrcPages('w', { host: 'wedn.net' })).toBe('w@wedn.net')
-  expect(() => lrcPages(100)).toThrow('Expected a string, got number')
-})
